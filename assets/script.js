80bca6cc38674155919fff36111e674ce54bc2e3
Luigi <128727133+luigibuono@users.noreply.github.com>
2023-07-28 12:48:32 +0200

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); 
+}
+
+
+var navToggle = $("#navToggle");
+var carouselText =$(".carouselText");
+
+navToggle.on("click",function(){
+    if(carouselText.hasClass("carouselTextPosition")===true){
+        carouselText.removeClass("carouselTextPosition")
+    } else { 
+        carouselText.addClass("carouselTextPosition");
+    }
+});
